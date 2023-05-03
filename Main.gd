c499c8b903466478cbbb98d64e1a933bb80bf416
Erik Ring-Walters <erik.ring.walters@gmail.com>
2023-05-02 22:45:05 -0700

    music

--- Main.gd
+++ Main.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide()
 
 func _on_mob_timer_timeout():
 
@@ -20,3 +22,8 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		get_tree().reload_current_scene()
